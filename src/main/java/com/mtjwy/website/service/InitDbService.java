58deee45c39718a751b501040a4e658ff1a94710
mtjwy <wuyucsu@gmail.com>
2015-10-27 22:39:39 -0500

    use bcrypt to encrypt password

--- src/main/java/com/mtjwy/website/service/InitDbService.java
+++ src/main/java/com/mtjwy/website/service/InitDbService.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -48,7 +49,8 @@ public class InitDbService {
 		
 		WebUser userAdmin = new WebUser();
 		userAdmin.setName("admin");
-		userAdmin.setPassword("123");
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		userAdmin.setPassword(encoder.encode("123"));
 		List<Role> roles = new ArrayList<Role>();
 		roles.add(roleUser);
 		roles.add(roleAdmin);
